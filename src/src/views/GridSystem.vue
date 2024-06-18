a6aadc157656c267e737af50df5005b3633b8e78
Leo <fkffksk20@gmail.com>
2024-06-18 23:31:11 +0900

    first commit

---
+++ src/src/views/GridSystem.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>Grid System</h1>
+    <!-- 여기에 GridSystem 컴포넌트의 템플릿 코드를 작성하세요 -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'GridSystem',
+  // 여기에 컴포넌트 로직을 추가하세요
+}
+</script>
+
+<style scoped>
+/* 여기에 컴포넌트의 스타일을 추가하세요 */
+</style>
