7fcb23ea8aac4e901a483bf110d9b59f7bd2a2bb
Leo <fkffksk20@gmail.com>
2024-06-20 11:34:26 +0900

    Revert "first commit"
    This reverts commit 3210782b81c6dd8e4124166300162a101d83eb1f.

--- src/App.vue
+++ src/App.vue
@@ -1,83 +1,28 @@
 <template>
-  <v-app>
-    <v-app-bar
-        app
-        color="primary"
-        dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-            alt="Vuetify Logo"
-            class="shrink mr-2"
-            contain
-            src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-            transition="scale-transition"
-            width="40"
-        />
-
-        <v-img
-            alt="Vuetify Name"
-            class="shrink mt-1 hidden-sm-and-down"
-            contain
-            min-width="100"
-            src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-            width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-          href="https://github.com/vuetifyjs/vuetify/releases/latest"
-          target="_blank"
-          text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
-    <v-navigation-drawer app>
-      <v-list
-          dense
-          nav
-        >
-      <v-list-item
-          v-for="item in items"
-          :key="item.title"
-          link
-          :to="item.to"
-      >
-        <v-list-item-icon>
-          <v-icon>{{ item.icon }}</v-icon>
-        </v-list-item-icon>
-
-        <v-list-item-content>
-          <v-list-item-title>{{ item.title }}</v-list-item-title>
-        </v-list-item-content>
-      </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-
-    <v-main>
-      <router-view/>
-    </v-main>
-  </v-app>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
 </template>
 
 <script>
+import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-
-  components: {},
-
-  data: () => ({
-    items: [
-      { title: 'DashBoard', icon: 'mdi-view-dashboard', to: '/' },
-      { title: 'Grid System', icon: 'mdi-view-dashboard', to: '/grid-system' },
-    ],
-    right: null,
-  }),
-};
+  components: {
+    HelloWorld
+  }
+}
 </script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
