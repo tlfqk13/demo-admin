0806cf38216f57adad0d3d06cb63f7ed509eb75c
Leo <fkffksk20@gmail.com>
2024-06-20 11:31:37 +0900

    first commit

---
+++ src/main/java/com/example/baskorea/BasKoreaApplication.java
@@ -0,0 +1,13 @@
+package com.example.baskorea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasKoreaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasKoreaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/baskorea/BasKoreaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.baskorea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasKoreaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
