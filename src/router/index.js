ef0b074d5c1eccb51b8d9477b33505acb41ecc40
Leo <fkffksk20@gmail.com>
2024-06-20 10:49:37 +0900

    * 데모 개발

--- src/router/index.js
+++ src/router/index.js
@@ -2,14 +2,13 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 
 import DashBoard from '@/views/DashBoard.vue'
-import GridSystem from '@/views/GridSystem.vue'
-import GridListPage from '@/views/GridListPage.vue'
-import Breakpoints from '@/views/Breakpoints.vue'
-import Typography from '@/views/Typography.vue'
-import Table from '@/views/Table.vue'
 import Forms from '@/views/Forms.vue'
 import Buttons from '@/views/Buttons.vue'
-import Icons from '@/views/Icons.vue'
+import Inquiry from "@/views/InquiryView.vue";
+import EstimateManagement from "@/views/EstimateManagement.vue";
+import CustomerCode from "@/views/CustomerCode.vue";
+import SupplierCode from "@/views/SupplierCode.vue";
+import ShipCode from "@/views/ShipCode.vue";
 
 Vue.use(VueRouter)
 
@@ -18,31 +17,6 @@ const routes = [
     path: "/",
     name: 'Dashboard',
     component: DashBoard
-  },
-  {
-    path: "/grid-system",
-    name: 'GridSystem',
-    component: GridSystem
-  },
-  {
-    path: "/grid-list-page",
-    name: 'GridListPage',
-    component: GridListPage
-  },
-  {
-    path: "/breakpoints",
-    name: 'Breakpoints',
-    component: Breakpoints
-  },
-  {
-    path: "/typography",
-    name: 'Typography',
-    component: Typography
-  },
-  {
-    path: "/table",
-    name: 'table',
-    component: Table
   },
   {
     path: "/forms",
@@ -55,9 +29,29 @@ const routes = [
     component: Buttons
   },
   {
-    path: "/icons",
-    name: 'icons',
-    component: Icons
+    path: "/inquiry",
+    name: 'inquiry',
+    component: Inquiry
+  },
+  {
+    path: "/estimateManagement",
+    name: 'estimateManagement',
+    component: EstimateManagement
+  },
+  {
+    path: "/customer-code",
+    name: 'customer-code',
+    component: CustomerCode
+  },
+  {
+    path: "/supplier-code",
+    name: 'supplier-code',
+    component: SupplierCode
+  },
+  {
+    path: "/ship-code",
+    name: 'ship-code',
+    component: ShipCode
   },
 ]
 
