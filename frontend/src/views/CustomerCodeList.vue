2932e710238ca1987d92a69a922bcea995edfb94
Leo <fkffksk20@gmail.com>
2024-06-20 14:51:18 +0900

    first commit

---
+++ frontend/src/views/CustomerCodeList.vue
@@ -0,0 +1,106 @@
+<template>
+  <v-card>
+    <v-card-title>
+      <v-icon class="mr-2" color="green" size="32">mdi-invoice-arrow-right-outline</v-icon>
+      매출처 코드 선택
+    </v-card-title>
+    <v-card-text>
+      <v-data-table
+          :headers="headers"
+          :items="filteredCustomers"
+          class="elevation-1 rounded-table-content"
+          item-key="code"
+          dense
+          @click:row="selectCustomer"
+          :items-per-page="15"
+      >
+        <template v-slot:top>
+          <v-toolbar flat>
+            <v-toolbar-title>매출처 코드 목록</v-toolbar-title>
+            <v-divider class="mx-4" inset vertical></v-divider>
+            <v-spacer></v-spacer>
+            <v-text-field
+                v-model="search"
+                append-icon="mdi-magnify"
+                label="검색"
+                single-line
+                hide-details
+            ></v-text-field>
+          </v-toolbar>
+        </template>
+      </v-data-table>
+    </v-card-text>
+    <v-card-actions>
+      <v-spacer></v-spacer>
+      <v-btn color="primary" @click="closeDialog">닫기</v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: 'CustomerCodeList',
+  data() {
+    return {
+      search: '',
+      headers: [
+        { text: '코드', value: 'code', width: '100px' },
+        { text: '사업자명', value: 'businessName', width: '200px' },
+        { text: '연락처', value: 'phone', width: '200px' },
+        { text: '담당자', value: 'contactPerson', width: '150px' },
+        { text: 'E-MAIL', value: 'email', width: '200px' },
+        { text: '주소', value: 'address', width: '300px' },
+        { text: '기타정보', value: 'extraInfo', width: '200px' },
+      ],
+      customers: [
+        {
+          code: 'AR',
+          businessName: 'ARYA TANKERS PVT LTD',
+          contactPerson: 'Mr. Yogesh',
+          phone: '+91 22 49229009',
+          email: 'yogesh.shinde@arya.in',
+          address: 'Marathon Futurex, B-501, Mafatlal Mills, N.M. Joshi Marg, Lower Parel, Mumbai',
+          supplierCode: 'SUP123',
+          contact: '+91 22 49229009',
+          extraInfo: '추가 정보 예시'
+        },
+        ...Array.from({length: 29}, (_, i) => ({
+          code: `CUST${i + 1}`,
+          businessName: `Business ${i + 1}`,
+          contactPerson: `Contact ${i + 1}`,
+          phone: `+91 22 4000000${i + 1}`,
+          email: `email${i + 1}@business.com`,
+          address: `Address ${i + 1}`,
+          supplierCode: `SUP${i + 1}`,
+          contact: `+91 22 4000000${i + 1}`,
+          extraInfo: `Extra info ${i + 1}`
+        }))
+      ],
+    };
+  },
+  computed: {
+    filteredCustomers() {
+      const searchTerm = this.search.toLowerCase();
+      return this.customers.filter(customer =>
+          Object.values(customer).some(value =>
+              String(value).toLowerCase().includes(searchTerm)
+          )
+      );
+    }
+  },
+  methods: {
+    selectCustomer(customer) {
+      this.$emit('select-customer-code', customer);
+    },
+    closeDialog() {
+      this.$emit('close');
+    },
+  },
+};
+</script>
+
+<style scoped>
+.rounded-table-content {
+  border-radius: 16px !important;
+}
+</style>
--- frontend/src/views/InquiryView.vue
+++ frontend/src/views/InquiryView.vue
@@ -13,51 +13,51 @@
                 </v-col>
                 <v-col cols="3">
                   <v-menu
-                    ref="creationDateMenu"
-                    v-model="creationDateMenu"
-                    :close-on-content-click="false"
-                    :nudge-right="40"
-                    transition="scale-transition"
-                    offset-y
-                    min-width="290px"
+                      ref="creationDateMenu"
+                      v-model="creationDateMenu"
+                      :close-on-content-click="false"
+                      :nudge-right="40"
+                      transition="scale-transition"
+                      offset-y
+                      min-width="290px"
                   >
                     <template v-slot:activator="{ on, attrs }">
                       <v-text-field
-                        v-bind="attrs"
-                        v-on="on"
-                        label="작성일자"
-                        v-model="creationDate"
-                        readonly
+                          v-bind="attrs"
+                          v-on="on"
+                          label="작성일자"
+                          v-model="creationDate"
+                          readonly
                       ></v-text-field>
                     </template>
                     <v-date-picker
-                      v-model="creationDate"
-                      @input="creationDateMenu = false"
+                        v-model="creationDate"
+                        @input="creationDateMenu = false"
                     ></v-date-picker>
                   </v-menu>
                 </v-col>
                 <v-col cols="3">
                   <v-menu
-                    ref="shippingDateMenu"
-                    v-model="shippingDateMenu"
-                    :close-on-content-click="false"
-                    :nudge-right="40"
-                    transition="scale-transition"
-                    offset-y
-                    min-width="290px"
+                      ref="shippingDateMenu"
+                      v-model="shippingDateMenu"
+                      :close-on-content-click="false"
+                      :nudge-right="40"
+                      transition="scale-transition"
+                      offset-y
+                      min-width="290px"
                   >
                     <template v-slot:activator="{ on, attrs }">
                       <v-text-field
-                        v-bind="attrs"
-                        v-on="on"
-                        label="선적일자"
-                        v-model="shippingDate"
-                        readonly
+                          v-bind="attrs"
+                          v-on="on"
+                          label="선적일자"
+                          v-model="shippingDate"
+                          readonly
                       ></v-text-field>
                     </template>
                     <v-date-picker
-                      v-model="shippingDate"
-                      @input="shippingDateMenu = false"
+                        v-model="shippingDate"
+                        @input="shippingDateMenu = false"
                     ></v-date-picker>
                   </v-menu>
                 </v-col>
@@ -67,32 +67,32 @@
               </v-row>
               <v-row>
                 <v-col cols="3">
-                  <v-select
-                    :items="shippingCompanies"
-                    label="선적자"
-                    v-model="shippingCompany"
-                  ></v-select>
+                  <v-text-field
+                      label="선적자"
+                      v-model="shippingCompany"
+                      append-icon="mdi-magnify"
+                      readonly
+                      @click:append="openShipCodeDialog"
+                  ></v-text-field>
+                </v-col>
+                <v-col cols="3">
+                  <v-text-field
+                      label="매출처"
+                      v-model="customer"
+                      append-icon="mdi-magnify"
+                      readonly
+                      @click:append="openCustomerCodeDialog"
+                  ></v-text-field>
                 </v-col>
                 <v-col cols="3">
                   <v-select
-                    :items="hullNumbers"
-                    label="Hull No"
-                    v-model="hullNumber"
+                      :items="managers"
+                      label="담당자"
+                      v-model="manager"
                   ></v-select>
                 </v-col>
                 <v-col cols="3">
-                  <v-select
-                    :items="managers"
-                    label="매출처"
-                    v-model="salesPerson"
-                  ></v-select>
-                </v-col>
-                <v-col cols="3">
-                  <v-select
-                    :items="managers"
-                    label="담당자"
-                    v-model="manager"
-                  ></v-select>
+                  <v-text-field label="Hull No." v-model="hullNumbers"></v-text-field>
                 </v-col>
               </v-row>
               <v-row>
@@ -102,8 +102,8 @@
                 <v-col cols="3">
                   <v-text-field label="비고" v-model="remarks"></v-text-field>
                 </v-col>
-                <v-col cols="6">
-                  <v-btn color="primary" @click="search">추가</v-btn>
+                <v-col cols="6" class="text-right">
+                  <v-btn color="primary" @click="addItem">추가</v-btn>
                 </v-col>
               </v-row>
             </v-form>
@@ -115,20 +115,37 @@
     <!-- INQUIRY 그리드 추가 -->
     <v-row class="mt-5">
       <v-col>
-        <InquiryGrid />
+        <InquiryGrid
+            :items="inquiryItems"
+            @update:item="updateItem"
+            @delete:item="deleteItem"
+        />
       </v-col>
     </v-row>
+
+    <!-- 선적자 코드 모달 -->
+    <v-dialog v-model="shipCodeDialog" max-width="800px">
+      <ShipCodeList @select-ship-code="selectShipCode" @close="shipCodeDialog = false" />
+    </v-dialog>
+
+    <!-- 매출처 코드 모달 -->
+    <v-dialog v-model="customerCodeDialog" max-width="800px">
+      <CustomerCodeList @select-customer-code="selectCustomerCode" @close="customerCodeDialog = false" />
+    </v-dialog>
   </v-container>
 </template>
 
 <script>
 import InquiryGrid from "@/views/InquiryGrid.vue";
-
+import ShipCodeList from "@/views/ShipCodeList.vue";
+import CustomerCodeList from "@/views/CustomerCodeList.vue";
 
 export default {
   name: 'InquiryView',
   components: {
     InquiryGrid,
+    ShipCodeList,
+    CustomerCodeList,
   },
   data() {
     return {
@@ -139,23 +156,74 @@ export default {
       creationDateMenu: false,
       shippingDateMenu: false,
       shippingCompany: '',
+      customer: '',
       hullNumber: '',
       salesPerson: '',
       manager: '',
       refNumber: '',
       remarks: '',
-      status: {
-        estimate: false,
-        order: false,
-        invoice: false,
-        sales: false,
-        settlement: false,
-        payment: false,
-      },
-      shippingCompanies: ['Company A', 'Company B', 'Company C'],
-      hullNumbers: ['001', '002', '003'],
+      shipCodeDialog: false,
+      customerCodeDialog: false,
+      inquiryItems: [],
+      hullNumbers: '',
       managers: ['Manager A', 'Manager B', 'Manager C'],
     };
+  },
+  methods: {
+    addItem() {
+      const newItem = {
+        id: this.inquiryItems.length + 1,
+        documentNumber: this.documentNumber,
+        creationDate: this.creationDate,
+        shippingDate: this.shippingDate,
+        exchangeRate: this.exchangeRate,
+        shippingCompany: this.shippingCompany,
+        customer: this.customer,
+        hullNumber: this.hullNumber,
+        salesPerson: this.salesPerson,
+        manager: this.manager,
+        refNumber: this.refNumber,
+        remarks: this.remarks,
+      };
+      this.inquiryItems.push(newItem);
+      this.clearForm();
+    },
+    updateItem({index, item}) {
+      this.inquiryItems.splice(index, 1, item);
+    },
+    deleteItem(item) {
+      const index = this.inquiryItems.indexOf(item);
+      if (index > -1) {
+        this.inquiryItems.splice(index, 1);
+      }
+    },
+    clearForm() {
+      this.documentNumber = '';
+      this.creationDate = '';
+      this.shippingDate = '';
+      this.exchangeRate = '';
+      this.shippingCompany = '';
+      this.customer = '';
+      this.hullNumber = '';
+      this.salesPerson = '';
+      this.manager = '';
+      this.refNumber = '';
+      this.remarks = '';
+    },
+    openShipCodeDialog() {
+      this.shipCodeDialog = true;
+    },
+    openCustomerCodeDialog() {
+      this.customerCodeDialog = true;
+    },
+    selectShipCode(shipCode) {
+      this.shippingCompany = shipCode.businessName;
+      this.shipCodeDialog = false;
+    },
+    selectCustomerCode(customer) {
+      this.customer = customer.businessName;
+      this.customerCodeDialog = false;
+    },
   },
 };
 </script>
@@ -177,5 +245,22 @@ export default {
 
 .v-col > .v-input--selection-controls__input {
   margin-top: 0 !important;
+}
+
+.elevation-3 {
+  box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1);
+}
+
+.rounded-table {
+  border-radius: 16px;
+  overflow: hidden;
+}
+
+.rounded-table-content {
+  border-radius: 16px !important;
+}
+
+.rounded-card {
+  border-radius: 16px;
 }
 </style>
